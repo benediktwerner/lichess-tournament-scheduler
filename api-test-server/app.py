d6bca647a5d9c3a9a328bf4899eebc9e7c2d7b31
Benedikt Werner <1benediktwerner@gmail.com>
2022-07-20 08:52:36 +0200

    Add api test server

---
+++ api-test-server/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
+from time import time
+
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/api/token/test", methods=["POST"])
+def token_test() -> str:
+    token = request.data.decode()
+    return jsonify(
+        {
+            token: {
+                "userId": "benwerner",
+                "expires": int(time() * 1000) + 60 * 60 * 1000,
+                "scopes": "tournament:write",
+            }
+        }
+    )
