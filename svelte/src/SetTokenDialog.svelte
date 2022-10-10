0ef0881e13c935ef750c21239370c14fcdd57e0d
Benedikt Werner <1benediktwerner@gmail.com>
2022-10-10 10:09:24 +0200

    Update token creation URL to use new team:lead permission

--- svelte/src/SetTokenDialog.svelte
+++ svelte/src/SetTokenDialog.svelte
@@ -8,9 +8,8 @@
 
 Log into the Lichess account that should send the messages, then visit
 <a
-  href="https://lichess.org/account/oauth/token/create?scopes[]=team:write&description=Variant+Team+Tournament+Scheduler+Message+Token"
->
-  this page</a
+  href="https://lichess.org/account/oauth/token/create?scopes[]=team:lead&description=Variant+Team+Tournament+Scheduler+Message+Token"
+  >this page</a
 >, create the token, and paste it here:
 
 <br />
